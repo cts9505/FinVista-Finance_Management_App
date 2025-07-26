a92e10afd0e548d881993535c32cf0865d347848
Chaitanya Shinde <9chaitanyashinde@gmail.com>
2025-07-26 16:38:21 +0530

    Initial Update 7

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -437,7 +437,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Lax',
+            sameSite: 'None',
             maxAge: 24 * 60 * 60 * 1000
         });
 
@@ -611,7 +611,7 @@ export const googleAuth = async (req, res) => {
       res.cookie('token', token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'Lax',
+          sameSite: 'None',
           maxAge: 24 * 60 * 60 * 1000,
       });
 
